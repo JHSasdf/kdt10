803953945337aa51cc1e39a0766c045960db7ffa
JHSasdf <sgh4760@naver.com>
2023-11-22 14:00:19 +0900

    study:231112

--- back/07-3-express/app.js
+++ back/07-3-express/app.js
@@ -9,19 +9,27 @@ app.set('view engine', 'ejs');
 
 // 템플릿 엔진 파일을 (.ejs) 저장할 위치 등록
 app.set('views', './views');
-
+// set: 환경을 등록
 
 // static 미들웨어 등록 (정적 파일 로드 ex. css, js, img)
 // static이라는 실제 폴더를 static 이름으로 접근하겠다.
 // dirname == 해당 파일인 app.js 기준
 app.use('/asdf', express.static(__dirname + '/public'))
+
+
 // app.get(클라이언트가 요청한 경로, 해당 경로로 요청이 들어왔을 때 실행할 함수)
 app.get('/', function(req, res) {
-
     // view engine에 ejs를 사용하기로 했기 때문에 (line8) 확장자 불요, 써도 되긴 함.
-    res.render('index');
+    res.render('index', {btns : ['사과', '오렌지', '키위'], isLoggedIn: true});
 })
 
+app.get('/login', function(req, res) {
+    res.render('login');
+})
+
+app.get('/register', function(req, res) {
+    res.render('register');
+})
 app.listen(PORT, function() {
     console.log(`server listening on ${PORT}`) // 서버가 열리면 콜백함수 수행
 });
